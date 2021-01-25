7b8e106e08c7afb440c93854897fc2493c125e56
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-01-25 14:33:30 +0100

    Starts category menus rework

--- app/src/main/java/fr/isen/attia/androidrestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/attia/androidrestaurant/HomeActivity.kt
@@ -19,37 +19,19 @@ class HomeActivity : AppCompatActivity() {
         binding.menuBtnStarters.setOnClickListener{
             //Toast used for debugging only
             //Toast.makeText(this, "Starters button clicked !", Toast.LENGTH_SHORT).show()
-            showStartersMenu()
         }
         binding.menuBtnMainCourses.setOnClickListener{
             //Toast used for debugging only
             //Toast.makeText(this, "Main Courses button clicked !", Toast.LENGTH_SHORT).show()
-            showMainCoursesMenu()
         }
         binding.menuBtnDesserts.setOnClickListener{
             //Toast used for debugging only
             //Toast.makeText(this, "Desserts button clicked !", Toast.LENGTH_SHORT).show()
-            showDessertsMenu()
         }
     }
 
     override fun onDestroy() {
-        super.onDestroy()
         Log.i(TAG, "HomeActivity has been destroyed")
-    }
-
-    private fun showStartersMenu(){
-        val intent = Intent(this, StartersActivity::class.java)
-        startActivity(intent)
-    }
-    
-    private fun showDessertsMenu(){
-        val intent = Intent(this, DessertsActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun showMainCoursesMenu(){
-        val intent = Intent(this, MainCoursesActivity::class.java)
-        startActivity(intent)
+        super.onDestroy()
     }
 }
