bdddc595283ee1f30d0689ed4caf61aba533ccd3
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-01-29 14:36:28 +0100

    Adds image carousel

--- app/src/main/java/fr/isen/attia/androidrestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/attia/androidrestaurant/DetailActivity.kt
@@ -3,20 +3,28 @@ package fr.isen.attia.androidrestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
 import fr.isen.attia.androidrestaurant.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var food: SerializedFood
 
     private var id: Int? = 0
+    private lateinit var carousel: CarouselView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val food: SerializedFood = intent.getSerializableExtra("DISH") as SerializedFood
+        food = intent.getSerializableExtra("DISH") as SerializedFood
         Log.d("SERIALIZED_FOOD", food.toString())
+
+        populateCarousel()
 
         binding.dishPriceText.text = food.price + "€"
 
@@ -25,5 +33,19 @@ class DetailActivity : AppCompatActivity() {
             ingredientsList += "- $ingredient\n"
         }
         binding.ingredientsTextview.text = ingredientsList
+    }
+
+    private fun populateCarousel(){
+        carousel = binding.root.findViewById(R.id.carouselView)
+        carousel.pageCount = food.images?.size ?: 0
+
+        val imageListener: ImageListener = ImageListener{ i: Int, imageView: ImageView ->
+            val imgUrl = food.images?.get(i)
+            if(!imgUrl.isNullOrEmpty()){
+                Picasso.with(this).load(imgUrl).into(imageView)
+            }
+        }
+
+        carousel.setImageListener(imageListener)
     }
 }
