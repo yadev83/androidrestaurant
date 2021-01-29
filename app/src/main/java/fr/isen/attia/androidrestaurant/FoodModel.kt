3b6da053b11c3d635e3e7a4d05ad0c913f7fe7e6
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-01-29 11:48:28 +0100

    Adds a cache for the request

--- app/src/main/java/fr/isen/attia/androidrestaurant/FoodModel.kt
+++ app/src/main/java/fr/isen/attia/androidrestaurant/FoodModel.kt
@@ -20,6 +20,9 @@ class FoodModel(val id: Int, val name: String, var images: Array<String?>?, val 
         private var lastItemId = 0
         var currentFoods: MutableLiveData<ArrayList<FoodModel>> = MutableLiveData()
 
+        const val USER_PREFERENCES_NAME = "USER_PREFERENCES_NAME"
+        const val REQUEST_CACHE = "REQUEST_CACHE"
+
         fun createFoodList(numItems: Int){
             var foods = ArrayList<FoodModel>()
             for (i in 1..numItems){
@@ -38,43 +41,61 @@ class FoodModel(val id: Int, val name: String, var images: Array<String?>?, val 
         }
 
         fun gatherFoodFromApi(context: CategoryActivity, title: String){
-            val queue = Volley.newRequestQueue(context)
+            resultFromCache(context)?.let {
+                // La requete est en cache
+                parseResult(it, title)
+                context.disableLoadingAnimation()
+            } ?: run {
+                val queue = Volley.newRequestQueue(context)
 
-            val activity = context
+                val url = "http://test.api.catering.bluecodegames.com/menu"
+                val params = HashMap<String,String>()
+                params["id_shop"] = "1"
 
-            val url = "http://test.api.catering.bluecodegames.com/menu"
-            val params = HashMap<String,String>()
-            params["id_shop"] = "1"
+                val paramsJsonObject = JSONObject(params as Map<*, *>)
+                val request = JsonObjectRequest(Request.Method.POST, url, paramsJsonObject,
+                    Response.Listener{
+                            response ->
+                        run {
+                            cacheResult(context, response.toString())
+                            parseResult(response.toString(), title)
+                            context.disableLoadingAnimation()
+                        }
+                    },
+                    Response.ErrorListener { Log.i("FOOD", "API Request error") })
+                queue.add(request)
+            }
+        }
 
-            val paramsJsonObject = JSONObject(params as Map<*, *>)
-            val request = JsonObjectRequest(Request.Method.POST, url, paramsJsonObject,
-                Response.Listener{
-                    response ->
-                    run {
-                        var foodData: FoodData = Gson().fromJson<FoodData>(response.toString(), FoodData::class.java)
-                        //DEBUG LOG
-                        //Log.d("FOOD", foodData.toString())
-                        var foods = ArrayList<FoodModel>()
-                        currentFoods.value = foods
-                        foodData.data.forEach{category ->
-                            category.items.forEach {food ->
-                                food.id?.let { foodId ->
-                                    food.name_fr?.let { foodName ->
-                                        FoodModel(foodId, foodName, food.images, food.ingredients, food.prices.first().price)
-                                    }
-                                }?.let { foodModel ->
-                                    if(food.categ_name_fr == title){
-                                        foods.add(foodModel)
-                                        currentFoods.value = foods
-                                        activity.disableLoadingAnimation()
-                                    }
-                                }
-                            }
+        private fun parseResult(response: String, title: String){
+            var foodData: FoodData = Gson().fromJson<FoodData>(response.toString(), FoodData::class.java)
+            var foods = ArrayList<FoodModel>()
+            foodData.data.forEach{category ->
+                category.items.forEach {food ->
+                    food.id?.let { foodId ->
+                        food.name_fr?.let { foodName ->
+                            FoodModel(foodId, foodName, food.images, food.ingredients, food.prices.first().price)
+                        }
+                    }?.let { foodModel ->
+                        if(food.categ_name_fr == title){
+                            foods.add(foodModel)
+                            currentFoods.value = foods
                         }
                     }
-                },
-                Response.ErrorListener { Log.i("FOOD", "API Request error") })
-            queue.add(request)
+                }
+            }
+        }
+
+        private fun cacheResult(context: Context, response: String) {
+            val sharedPreferences = context.getSharedPreferences(USER_PREFERENCES_NAME, Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString(REQUEST_CACHE, response)
+            editor.apply()
+        }
+
+        private fun resultFromCache(context: Context): String? {
+            val sharedPreferences = context.getSharedPreferences(USER_PREFERENCES_NAME, Context.MODE_PRIVATE)
+            return sharedPreferences.getString(REQUEST_CACHE, null)
         }
     }
 }
