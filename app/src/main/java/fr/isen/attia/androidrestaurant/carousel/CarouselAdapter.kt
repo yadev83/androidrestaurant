dd012c3da2173bb7b48859ad0fb415741ae556fe
yadev83 <50366613+yadev83@users.noreply.github.com>
2021-02-01 17:35:25 +0100

    Adds comments for carousel code

--- app/src/main/java/fr/isen/attia/androidrestaurant/carousel/CarouselAdapter.kt
+++ app/src/main/java/fr/isen/attia/androidrestaurant/carousel/CarouselAdapter.kt
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
+/**
+ * @brief Adapts a list of images to a ViewPager carousel
+ * @param activity The parent activity
+ * @param items The list of images url to be used
+ */
 class CarouselAdapter(activity: AppCompatActivity, private val items: List<String?>) : FragmentStateAdapter(activity) {
+    /**
+     * @brief Returns the number of urls (images)
+     */
     override fun getItemCount(): Int {
         return items.count()
     }
 
+    /**
+     * @brief Creates a fragment for a given position in the list
+     * Supposing that we have a list of urls, each one is going to be a fragment containing an image
+     */
     override fun createFragment(position: Int): Fragment {
         return items[position]?.let { CarouselFragment.newInstance(it) } as Fragment
     }
--- app/src/main/java/fr/isen/attia/androidrestaurant/carousel/CarouselFragment.kt
+++ app/src/main/java/fr/isen/attia/androidrestaurant/carousel/CarouselFragment.kt
@@ -8,9 +8,23 @@ import android.view.ViewGroup
 import com.squareup.picasso.Picasso
 import fr.isen.attia.androidrestaurant.databinding.FragmentCarouselBinding
 
+/**
+ * @brief A fragment of our Carousel (using ViewPager2 view)
+ * It is a class that builds a fragment of the carousel using an image url.
+ * Picasso lib will load the image from the url and bind it to the fragment's image view
+ */
 class CarouselFragment : Fragment() {
+    /**
+     * @brief the binding is necessary to access inner elements of the view
+     */
     private lateinit var binding: FragmentCarouselBinding
 
+    /**
+     * @brief This override method will be called when the fragment view is created
+     * @param inflater The layout inflater to use
+     * @param container The parent container
+     * @return Returns the view in which to build our fragment
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,17 +33,29 @@ class CarouselFragment : Fragment() {
         return binding.root
     }
 
+    /**
+     * @brief onViewCreated will be called AFTER the fragment view is created
+     * @param view The freshly created view
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //Using picasso, we check for an empty URL and bind the frag with the image if possible
         val url = arguments?.getString(URL)
         if(!url.isNullOrEmpty()){
             Picasso.with(context).load(url).into(binding.fragImg)
         }
     }
 
+    /**
+     * @brief An inner companion object to allow for a static method
+     */
     companion object{
         const val URL = "url"
+
+        /**
+         * @brief Creates a new carousel fragment from outside the CarouselFragment class
+         */
         fun newInstance(url: String): CarouselFragment?{
             return CarouselFragment().apply{ arguments = Bundle().apply{ putString(URL, url) } }
         }
